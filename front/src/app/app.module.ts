6d08907b125080979dc16325d021b1135fbc5652
hwan517h1 <hwan517h1@gmail.com>
2018-10-16 20:18:17 +0900

    Create a component called 'qr'

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './/app-routing.module';
 import { SignComponent } from './sign/sign.component';
 import { CreateComponent } from './create/create.component';
 import { SelectComponent } from './select/select.component';
+import { QrComponent } from './qr/qr.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignComponent,
     CreateComponent,
-    SelectComponent
+    SelectComponent,
+    QrComponent
   ],
   imports: [
     BrowserModule,
