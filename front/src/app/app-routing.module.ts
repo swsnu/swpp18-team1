5c4b8b1a8fc90a71d72c9d28f519c8b5a6028c93
hwan517h1 <hwan517h1@gmail.com>
2018-10-16 20:27:37 +0900

    Update a routing module

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { SignComponent } from './sign/sign.component';
+import { CreateComponent } from './create/create.component';
+import { SelectComponent } from './select/select.component';
+import { QrComponent } from './qr/qr.component';
+import { ChannelComponent } from './channel/channel.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/sign', pathMatch: 'full' },
-  { path: 'sign'},
-  { path: 'create'},
-  { path: 'select'},
-  { path: 'qr'},
-  { path: 'channel'},
+  { path: 'sign', component: SignComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'select', component: SelectComponent },
+  { path: 'qr', component: QrComponent },
+  { path: 'channel', component: ChannelComponent},
   { path: '**', redirectTo: '/sign' }
 ];
 
