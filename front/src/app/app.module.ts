7f27f11e42d297f9b35be357b240f83ef1800e6f
hwan517h1 <hwan517h1@gmail.com>
2018-10-16 20:18:02 +0900

    Create a component called 'select'

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { SignComponent } from './sign/sign.component';
 import { CreateComponent } from './create/create.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignComponent,
-    CreateComponent
+    CreateComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
