5ecf595e01d9cf07b5a8b1e03fe774039de69588
donguk <uk6342@gmail.com>
2018-10-18 12:37:14 +0900

    Fix snippet model memeber

--- front/src/app/model/snippet.ts
+++ front/src/app/model/snippet.ts
@@ -1,9 +1,6 @@
-import { DirectMessageComponent } from '../app/direct-message/direct-message.component';
-import { ChannelComponent } from '../app/channel/channel.component';
-
 export class Snippet {
     user_id: number;
     snippetable_id: string;
-    snippetable_type: DirectMessageComponent | ChannelComponent;
+    snippetable_type: string;
     content: Text;
 }
