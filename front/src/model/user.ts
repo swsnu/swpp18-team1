c5f1651d65ed294c1f0079ed58e332b765816eaa
hwan517h1 <hwan517h1@gmail.com>
2018-11-04 15:00:14 +0900

    Add key

--- front/src/model/user.ts
+++ front/src/model/user.ts
@@ -1,7 +1,11 @@
 export class User {
+    // for manager
     is_manager: boolean;
     email: string;
     password: string;
+    key: string;
+
+    // for user
     nickname: string;
     profile_image: string;
 }
