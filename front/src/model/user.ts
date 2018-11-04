656e12394e12147812d27817bbc7c7fd3bfce738
hwan517h1 <hwan517h1@gmail.com>
2018-11-04 17:25:22 +0900

    Change the attribute of user

--- front/src/model/user.ts
+++ front/src/model/user.ts
@@ -1,11 +1,13 @@
 export class User {
-    // for manager
+    id?: number;
     is_manager: boolean;
-    email: string;
-    password: string;
-    key: string;
+
+    // for manager
+    email?: string;
+    password?: string;
+    key?: string;
 
     // for user
-    nickname: string;
-    profile_image: string;
+    nickname?: string;
+    profile_picture?: string;
 }
