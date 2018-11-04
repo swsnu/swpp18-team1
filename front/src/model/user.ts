9d4aa443749c9d9c29836cccd39331d86d1dcc17
hwan517h1 <hwan517h1@gmail.com>
2018-11-04 17:57:16 +0900

    Merge branches

--- front/src/model/user.ts
+++ front/src/model/user.ts
@@ -1,4 +1,5 @@
 export class User {
+<<<<<<< .merge_file_F0ONnd
 <<<<<<< .merge_file_dKPBbW
     id?: number;
     is_manager: boolean;
@@ -12,10 +13,15 @@ export class User {
     nickname?: string;
     profile_picture?: string;
 =======
+=======
+>>>>>>> .merge_file_BqzU32
     is_manager: boolean;
     email: string;
     password: string;
     nickname: string;
     profile_image: string;
+<<<<<<< .merge_file_F0ONnd
 >>>>>>> .merge_file_lXbArq
+=======
+>>>>>>> .merge_file_BqzU32
 }
