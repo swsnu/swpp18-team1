5e9c9d147d599fa294f8cc5ddb10becdc92a5c28
donguk <uk6342@gmail.com>
2018-11-11 22:38:46 +0900

    make noti when user is connected

--- back/chat/consumers.py
+++ back/chat/consumers.py
@@ -4,18 +4,23 @@ import json
 from .token_auth import TokenAuth, InvalidToken
 from channels.db import database_sync_to_async
 
+## TODO soket protocol class
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.channel_hash= self.scope['url_route']['kwargs']['channel_hash']
-        token = self.scope['url_route']['kwargs']['token']
-        try:
-            self.user = TokenAuth.asyncGetUserFrom(token)
-        except InvalidToken as e:
-            return self.close()
 
         ## TODO check channel hash
         ## TODO set group name by channel id decoded from channel_hash
+        token = self.scope['url_route']['kwargs']['token']
+
+        try:
+            self.user = await TokenAuth.asyncGetUserFrom(token)
+        except InvalidToken as e:
+            ## send fail message
+            return self.close()
+
+        print(self.user)
         self.room_group_name = 'channel_' + self.channel_hash
 
         # Join room group
@@ -24,7 +29,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        # Send intro message to group
         await self.accept()
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'content': self.user.username + "님이 입장했습니다.",
+                'id': "notification",
+            }
+        )
+
 
     async def disconnect(self, close_code):
         # Join room group
@@ -38,6 +53,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         content = text_data_json['content']
         id = text_data_json['id']
+
 
         # Send message to room group
         await self.channel_layer.group_send(
