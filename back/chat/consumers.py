3edb394439232021b0c068eb052fd89338d04a48
donguk <uk6342@gmail.com>
2018-11-11 17:47:57 +0900

    seprate token_auth logic

--- back/chat/consumers.py
+++ back/chat/consumers.py
@@ -5,6 +5,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+        ## TODO get token
 
         # Join room group
         await self.channel_layer.group_add(
