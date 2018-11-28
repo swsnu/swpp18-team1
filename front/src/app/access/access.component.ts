b561143907157b0d42f71cd49cc1b4303d0d7de3
donguk <uk6342@gmail.com>
2018-11-28 21:55:25 +0900

    make random name generator

--- front/src/app/access/access.component.ts
+++ front/src/app/access/access.component.ts
@@ -15,6 +15,7 @@ export class AccessComponent implements OnInit {
 
   channelHash: number;
   channelTitle: string = "";
+  user: User = new User({image: "defaultImage"})
 
   constructor(
     private userService: UserService,
@@ -28,18 +29,44 @@ export class AccessComponent implements OnInit {
     this.channelService.getChannel(this.channelHash).then((channel) => {
       this.channelTitle = channel.title
     })
+    this.user.username = this.nameRecommender()
   }
 
-  access(image: string, username: string): boolean {
+  access(): boolean {
 
-    image = image.trim();
-    username = username.trim();
-    if(!image) { return; }
-    if(!username) { return; }
+    if(!this.user.image.trim()) { return; }
+    if(!this.user.username.trim()) { return; }
 
-    this.userService.createUser(this.channelHash, {image, username} as Partial<User>)
+    this.userService.createUser(this.channelHash, {image: this.user.image, username: this.user.username} as Partial<User>)
       .then(() => {
           this.router.navigate([`/channel/${this.channelHash}`]);
       });
   }
+
+  nameRecommender(): string {
+    const names = [
+      "물 먹는 하마",
+      "목이 아픈 기린",
+      "맨날 짖는 강아지",
+      "턱이 아픈 악어",
+      "꿀 먹는 곰",
+      "질겅이는 송아지",
+      "하품하는 고양이",
+      "달리는 호랑이",
+      "멍 때리는 사자",
+      "눈이 침침한 독수리",
+      "다리 아픈 말",
+      "배 아픈 돼지",
+      "목이 쑤신 비둘기",
+      "추운 이구아나",
+      "부리가 무거운 펠리컨",
+      "놀고 있는 개미",
+      "일하는 배짱이",
+      "늦잠 잔 닭",
+      "넘어진 병아리",
+      "자빠진 오리",
+    ]
+    return names[Math.floor(Math.random() * names.length)];
+  }
+
 }
--- front/src/model/user.ts
+++ front/src/model/user.ts
@@ -1,11 +1,16 @@
 export class User {
-    id?: number;
-    username?: string;
+  id?: number;
+  username?: string;
 
-    // for manager
-    password?: string;
-    key?: string;
+  // for manager
+  password?: string;
+  key?: string;
 
-    // for user
-    image?: string;
+  // for user
+  image?: string;
+
+  public constructor(obj: Partial<User> = {}) {
+    Object.assign(this, obj);
+  }
+
 }
