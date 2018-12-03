122fb8a94c229a629581276868982538deee2e7a
hwan517h1 <hwan517h1@gmail.com>
2018-12-04 03:55:13 +0900

    Make first successful testing case

--- front/src/service/user.service.spec.ts
+++ front/src/service/user.service.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
       providers: [UserService]
     });
   });
